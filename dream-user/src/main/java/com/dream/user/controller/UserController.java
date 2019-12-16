0a96b82857854ef23bb0c4808cff7f4888eac64d
DreamCats <maifeng_cat@outlook.com>
2019-12-16 20:50:23 +0800

    测试UserController

--- dream-user/src/main/java/com/dream/user/controller/UserController.java
+++ dream-user/src/main/java/com/dream/user/controller/UserController.java
@@ -8,8 +8,13 @@
 package com.dream.user.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.dream.commons.result.ResponseData;
+import com.dream.commons.result.ResponseUtil;
 import com.dream.user.IUserService;
+import com.dream.user.dto.UserRequest;
+import com.dream.user.dto.UserResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +26,13 @@ public class UserController {
     @GetMapping("/test")
     public String hello() {
         return userService.Hello();
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseData searchUserById(@PathVariable(name = "id")Integer id) {
+        UserRequest req = new UserRequest();
+        UserResponse res = new UserResponse();
+        res = userService.userById(req);
+        return new ResponseUtil<Object>().setData(res);
     }
 }
