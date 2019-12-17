70907147314486878eda41cdba659b64c38cab74
DreamCats <maifeng_cat@outlook.com>
2019-12-17 23:26:46 +0800

    添加swagger

--- dream-user/src/main/java/com/dream/user/controller/UserController.java
+++ dream-user/src/main/java/com/dream/user/controller/UserController.java
@@ -15,17 +15,22 @@ import com.dream.user.constatns.SysRetCodeConstants;
 import com.dream.user.dto.UserListResponse;
 import com.dream.user.dto.UserRequest;
 import com.dream.user.dto.UserResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "UserController", description = "用户控制器")
 public class UserController {
 
     @Reference
     private IUserService userService;
 
     @GetMapping("/test")
+    @ApiOperation("测试接口")
     public String hello() {
         return userService.Hello();
     }
@@ -35,7 +40,9 @@ public class UserController {
      * @param id
      * @return
      */
+    @ApiOperation("根据id获取用户")
     @GetMapping("/user/{id}")
+    @ApiImplicitParam(name = "id", value = "用户id", paramType = "path", required = true)
     public ResponseData searchUserById(@PathVariable(name = "id")Integer id) {
         UserRequest req = new UserRequest();
         req.setId(id);
