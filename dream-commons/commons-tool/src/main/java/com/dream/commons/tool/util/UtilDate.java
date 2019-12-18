87f5d564757144eeff0f643f4f6efb6761ed8a94
DreamCats <maifeng_cat@outlook.com>
2019-12-18 20:25:04 +0800

    创建时间工具类

---
+++ dream-commons/commons-tool/src/main/java/com/dream/commons/tool/util/UtilDate.java
@@ -0,0 +1,21 @@
+/**
+ * @program dream
+ * @description: 时间工具类
+ * @author: mf
+ * @create: 2019/12/18 20:23
+ */
+
+package com.dream.commons.tool.util;
+
+public class UtilDate {
+
+    private final static String FORMAT_PATTERN1="yyyy-MM-dd HH:mm:ss";
+
+    private final static String FORMAT_PATTERN2="yyyyMMddHHmmss";
+
+    private final static String FORMAT_PATTERN3="yyyy-MM-dd";
+
+    private final static String FORMAT_PATTERN4="yyyyMMdd";
+
+
+}
